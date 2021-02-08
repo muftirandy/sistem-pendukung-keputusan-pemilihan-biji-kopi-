d1db3c770b2e6f4cca1f96866da6b8fd9d6bf884
muftirandy <69395029+muftirandy@users.noreply.github.com>
2021-02-08 14:32:32 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!-- <!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/view/index.php">
+<title>SB Admin 2</title>
+<script language="javascript">
+    window.location.href = "pages/view/index.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/view/index.php">/pages/view/index.php</a>
+</body>
+</html>
+ -->
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="refresh" content="0;url=pages/view/login.php">
+<title>SB Admin 2</title>
+<script language="javascript">
+    window.location.href = "pages/view/login.php"
+</script>
+</head>
+<body>
+Go to <a href="pages/view/login.php">/pages/view/index.php</a>
+</body>
+</html>
